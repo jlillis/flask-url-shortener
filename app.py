0ecdaeb0b07f6e0cd9f6904b17a9c32d06cf1c61
John Lillis <JohnMichaelFFS@gmail.com>
2019-11-30 14:46:11 -0500

    Refactor app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import hashlib
 
 app = Flask(__name__)
 
-short_urls = {}
+short_urls = []
 
 @app.route('/', methods=("GET", "POST"))
 def index():
@@ -20,25 +20,24 @@ def index():
         # Attempt to validate the url
         if not validators.url(url_input, public=True):
             return render_template("index.html", error_msg="Invalid URL.")
-        
-        print("URL input: "+url_input)
 
         # Generate the md5 hash of the input URL
         url_hash = hashlib.md5(url_input.encode("utf-8")).hexdigest()
 
         # Use the first 6 characters of the MD5 digest as the shortened URL
-        short_urls[url_hash[:6]] = url_input
+        short_urls.append({"url": url_hash, "destination": url_input})
 
         return render_template("index.html", error_msg=url_hash[:6])
 
 
     return render_template("index.html")
 
-@app.route("/<short_url>")
-def redirect_short_url(short_url):
+@app.route("/<url>")
+def redirect_url(url):
     """Redirects the short URL to the input URL."""
-    for record in short_urls.keys():
-        if record == short_url:
-            return redirect(short_urls[record])
+    for record in short_urls:
+        if record.url == url:
+            return redirect(record.destination)
         
     return render_template("index.html", error_msg="Short URL not found.")
+
