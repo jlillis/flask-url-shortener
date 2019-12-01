9d8ecd3d4118c994693bc9e8ff224e41c9dba340
John Lillis <JohnMichaelFFS@gmail.com>
2019-11-30 19:48:20 -0500

    Improve code quality and UX

--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,38 @@
 from app import app, db
 from app.models import ShortURL
 from flask import render_template, request, redirect
+from sqlalchemy.exc import IntegrityError, OperationalError
+import time
 import validators
 import hashlib
-
-short_urls = []
 
 @app.route('/', methods=("GET", "POST"))
 def index():
     """Handle GET and POST requests to the index page."""
     if request.method == "POST":
-        # Try reading the input URL from the POST data
+        # Try reading the destination URL from the POST data
         # Flask will raise a KeyError if no URL was provided
         try:
-            url_input = request.form["urlInput"]
+            destination_url = request.form["destinationUrlInput"]
         except KeyError:
-            return render_template("index.html", error_msg="Invalid URL.")
+            return render_template("index.html", error_msg="Invalid destination URL.")
 
         # Attempt to validate the url
-        if not validators.url(url_input, public=True):
-            return render_template("index.html", error_msg="Invalid URL.")
+        if not validators.url(destination_url, public=True):
+            return render_template("index.html", error_msg="Invalid destination URL.")
 
         # Generate the md5 hash of the input URL
-        url_hash = hashlib.md5(url_input.encode("utf-8")).hexdigest()
+        url_hash = hashlib.md5(f"destination_url{time.clock()}".encode("utf-8")).hexdigest()
 
         # Use the first 6 characters of the MD5 digest as the shortened URL
-        short_url = ShortURL(identifier=url_hash[:8], destination=url_input, hits=0)
+        short_url = ShortURL(identifier=url_hash[:8], destination=destination_url, hits=0)
         db.session.add(short_url)
-        db.session.commit()
-        #short_urls.append({"url": url_hash, "destination": url_input})
+        try:
+            db.session.commit()
+        except (IntegrityError, OperationalError):
+            return render_template("index.html", error_msg="URL creation failed.")
 
-        return render_template("index.html", error_msg=short_url)
+        return render_template("index.html", created_url=f"{request.base_url}{short_url.identifier}")
 
 
     return render_template("index.html")
